e04d1cf16d88386f47e7a4202f367cf4e71251ed
Louislam09 <luis.martinez@newfold.com>
2024-08-06 08:24:00 -0400

    update: local config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo']
+//   };
+// };
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [".tsx", ".ts", ".js", ".json"],
+          root: ["."],
+          alias: {
+            components: "./components",
+          },
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
